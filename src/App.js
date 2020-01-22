9a289a46060415d59f7b16358179635db5fd1bd1
Florin DUMITRU <dumitruflorin19@yahoo.com>
2020-01-22 16:03:44 +0000

    final 1st day work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './florin.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <div id='container'>
-        <h1>Welcome to Florin`s digital home</h1>
-        <div className='navigation'>
-          <h1>This is the navigation</h1>
-        </div>
-        <div className='body'>
-          <h2>This is the main body</h2>
-        </div>
-        <div className='footer'>
-          <h2>This is the footer</h2>
-        </div>
+export default class extends React.Component {
+  render() {
+    return (
+      <div className="App">
+    <header>
+      <h1>Florin Dumitru</h1>
+      </header>
+    <body>
+      <nav>
+        <ul className='nav'>
+          <li><a href='home'>Home</a></li>
+          <li><a href='about-me'>About me</a></li>
+          <li><a href='projects'>Projects</a></li>
+          <li><a href='contact-me'>Contact me</a></li>
+        </ul>
+      </nav>
+      <main>
+        <article>
+          <h1>Welcome to my portfolio</h1>
+          <p>Lorem ipsum</p>
+          <p>Lorem ipsum</p>
+          <p>Lorem ipsum</p>
+          <p>Lorem ipsum</p>
+          <p>Lorem ipsum</p>
+          <p>Lorem ipsum</p>
+          <p>Lorem ipsum</p>
+        </article>
+      <footer>Done by Florin</footer>
+      </main>
+    </body>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
