5942eb37f9dfc5aa580ebe5be9c83f63bf9572d5
Florin DUMITRU <dumitruflorin19@yahoo.com>
2020-02-01 15:06:06 +0000

    Renaming the component from Menu to Navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Menu from './Menu/Menu';
+import Navigation from './Navigation/Navigation';
 
 export default class extends React.Component {
   render() {
     return (
       <div className="App">
-        <Menu />
+        <Navigation />
       </div>
     );
   }
---
+++ src/Navigation/Navigation.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Navigation.css';
+
+export default class Navigation extends React.Component {
+    constructor() {
+        super();
+        this.state = { NavigationExpanded: false };
+    }
+
+    switchState() {
+        this.setState({ NavigationExpanded: !this.state.NavigationExpanded});
+    }
+
+    render() {
+        return (
+            <div className='Navigation'>
+                <NavigationButton 
+                NavigationExpanded={this.state.NavigationExpanded} 
+                onNavigationClick={() => this.switchState()} />
+                <div className='Navigation-links'>
+                    <div className='Navigation-link'>Home</div>
+                    <div className='Navigation-link'>About me</div>
+                    <div className='Navigation-link'>Skills</div>
+                    <div className='Navigation-link'>Portfolio</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+function NavigationButton({ NavigationExpanded, onNavigationClick }) {
+    return (
+        <button 
+        onClick={() => onNavigationClick()}
+        className='NavigationButton'>
+            Navigation {NavigationExpanded ? 'Yes!' : 'No'}
+        </button>
+    );
+}
