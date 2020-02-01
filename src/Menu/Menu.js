351e18284c19f89d924087e4d8bdfd4c95e4aeb3
Florin DUMITRU <dumitruflorin19@yahoo.com>
2020-02-01 14:48:32 +0000

    Menu styling and button creation on Menu

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Menu.css';
 
 export default class Menu extends React.Component {
     constructor() {
@@ -6,9 +7,16 @@ export default class Menu extends React.Component {
         this.state = { isExpanded: false };
     }
 
+    switchState() {
+        this.setState({ NavigationExpanded: !this.state.NavigationExpanded});
+    }
+
     render() {
         return (
             <div className='Menu'>
+                <MenuButton 
+                NavigationExpanded={this.state.NavigationExpanded} 
+                onNavigationClick={() => this.switchState()} />
                 <div className='Menu-links'>
                     <div className='Menu-link'>Home</div>
                     <div className='Menu-link'>About me</div>
@@ -18,4 +26,14 @@ export default class Menu extends React.Component {
             </div>
         );
     }
+}
+
+function MenuButton({ NavigationExpanded, onNavigationClick }) {
+    return (
+        <button 
+        onClick={() => onNavigationClick()}
+        className='MenuButton'>
+            Navigation {NavigationExpanded ? 'Yes!' : 'No'}
+        </button>
+    );
 }
